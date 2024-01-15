7cc230d9d68ac1744ecc225ccf605488f8760376
nirajbharadva07 <nirajbharadva@gmail.com>
2024-01-15 17:59:02 +0530

    add new files

---
+++ video60-string/new.js
@@ -0,0 +1,24 @@
+console.log("hey this is string")
+let a = "niraj"
+console.log(a[1])
+console.log(a[2])
+console.log(a[-3])
+console.log(a[4])
+
+console.log(a.length)
+let real_name = "Harry"
+let friend = "Rohan"
+console.log("His name is " + real_name + " and his friends name is " + friend)
+console.log(`his name is ${real_name} and his friend name is ${friend}`)
+
+let b = "ShivamSh"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase()) 
+console.log(b.length) 
+console.log(b.slice(1, 5)) 
+console.log(b.slice(1)) 
+
+console.log(b.replace("Sh", "77"))
+console.log(b.concat(a, "Aishwariya", "Rahul", "Priya"))
+
+console.log(b)
